28803b673198105e9ec5d3d5eb6db02d8c2267a8
poojapaul07232 <poojapaul07232@gmail.com>
2023-02-08 12:38:25 +0530

    change form and media root

--- polling/customauth/forms.py
+++ polling/customauth/forms.py
@@ -8,7 +8,10 @@ class LoginForm(forms.Form):
     password = forms.CharField(max_length=63, widget=forms.PasswordInput)
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField(required=True)
+    password = forms.PasswordInput()
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', 'email', 'first_name', 'last_name')
+        fields = ('username', 'email','password1','password2')
         
